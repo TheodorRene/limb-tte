fddf5edf6cdaaa5a9d3d92ba9d72a3dcb7e30fc3
Theodor René Carlsen <theodor.rene@gmail.com>
2021-11-13 17:18:53 +0100

    finish mvp

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                   [ring/ring-core "1.9.4"]
-                  [ring/ring-jetty-adapter "1.9.4"]]
+                  [ring/ring-jetty-adapter "1.9.4"]
+                  [com.github.seancorfield/next.jdbc "1.2.737"]
+                  [org.xerial/sqlite-jdbc "3.36.0.1"]
+                  [compojure "1.6.2"]]
   :main ^:skip-aot limbøtte.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
--- src/limbøtte/core.clj
+++ src/limbøtte/core.clj
@@ -1,33 +1,104 @@
 (ns limbøtte.core
   (:require [ring.adapter.jetty :as j]
-            [ring.middleware.params :as p])
+            [ring.middleware.params :as p]
+            [next.jdbc :as jdbc]
+            [next.jdbc.result-set :as rs]
+            [compojure.core :refer [defroutes GET POST]]
+            )
   )
 
 (def plaintext {"Content-Type" "text/plain"})
 
-(defn retrieve-paste-id [params]
-  (get params "id"))
 
-;; Here actually do the database query
-(defn get-paste [stuff] (str stuff))
+;; DB stuff
+(defn db [name] {:dbtype "sqlite" :dbname name})
 
-(def request {:query-params {"id" "hei"}})
+(def ds
+  (jdbc/get-datasource (db "database.sqlite")))
+
+(def db-settings {:return-keys true
+                  :build-fn rs/as-unqualified-lower-maps})
+
+(defn do-query 
+  ([ds query]  (jdbc/execute! ds [query ]))
+  ([ds query param]  (jdbc/execute-one! ds [query param] db-settings))
+  )
+
+;; QUERIES
+(def drop-table "DROP TABLE paste")
+
+(def create-table "
+  CREATE TABLE IF NOT EXISTS paste (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      text TEXT)
+  ")
+
+(def get-paste-q 
+  "SELECT id, text FROM paste WHERE id=?")
+
+(def insert-paste-q "
+  INSERT INTO paste (text)
+  values
+  (?)
+  RETURNING 
+  id, text
+  ")
+;; INIT DB
+;;(do-query ds drop-table)
+(do-query ds create-table)
+(do-query ds insert-paste-q "gurba")
+;; END QUERIES
+
+(defn get-paste-db [ds id]
+ (:paste/text 
+   (do-query ds get-paste-q id)))
+
+(defn insert-paste-db [ds text]
+  (do-query ds insert-paste-q text))
 
 
-(defn get-paste-handler [req]
+(defn getHandler [id]
   {:status 200
    :header plaintext
-   :body (get-paste
-           (retrieve-paste-id 
-             (:query-params req)))})
-
-(:body 
-  (get-paste-handler request))
+   :body (str (get-paste-db ds id))
+   })
 
 
-;; Learn threading, most likely need to add more middleware
+(defn post-form [req]
+   "<div>
+      <h1>Limbøtte</h1>
+      <p> Gi meg ditt lim </p>
+      <form method=\"post\" action=\"\">
+       <input type=\"text\" name=\"name\" />
+       <input type=\"submit\" value\"submit\" />
+      </form>
+    </div>")
+
+
+(defn parseBody [req]
+  (let [{:keys [form-params]} req
+       param-p (get form-params "name")
+       ] param-p)
+  )
+
+(defn postHandler [req]
+  {:status 200
+   :header plaintext
+   :body (str
+             (insert-paste-db ds (parseBody req)))
+             
+   })
+
+(defroutes routes
+  (GET "/" [req] (post-form req))
+  (POST "/" req (postHandler req))
+  (GET "/:id" [id] (getHandler id))
+  )
+
+
 (def app
-  (p/wrap-params get-paste-handler))
+  (-> routes
+      p/wrap-params))
 
 (defn -main [& _]
-  (j/run-jetty app {:port 3000, :join? false}))
+  (j/run-jetty app {:port 3000}))
